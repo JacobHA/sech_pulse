3003793d8b1020c16234b6a5615f043f11073c9f
JacobHA <jacobhadamczyk@gmail.com>
2023-11-30 01:02:14 -0500

    minimize fidelity for smart magnetometer

--- dynamics.py
+++ dynamics.py
@@ -8,7 +8,7 @@ t_points = np.linspace(-10, 10, n_points)
 beta = 2*np.pi
 sys = TLS('sech', beta=beta, delta=0, phi=3*np.pi, t_points=t_points)
 print(sys.alpha/np.pi)
-sys.evolve()
+sys.evolve(noisy=True)
 sys.plot()
 sys.plot_bloch()
 
